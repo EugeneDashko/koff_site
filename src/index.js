7d36ae9822cfb18960ab7eba04a2ea71b8ae36a4
EugeneDashko <eugenedashko@yahoo.com>
2023-10-04 23:32:02 +0300

    conflict with swiper

--- src/index.js
+++ src/index.js
@@ -2,9 +2,29 @@ import './index.html';
 // import './page.html';
 import './index.scss';
 //new modules:
+import {Navigation, Thumbs}  from "swiper/modules";
+import Swiper from 'swiper';
+import 'swiper/css';
 
 
 
+const swiper = new Swiper(".product__slider-thumbnails", {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+   new Swiper(".product__slider-main", {
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".product__arrow_next",
+      prevEl: ".product__arrow_prev",
+    },
+    modules: [Navigation, Thumbs],
+    thumbs: {
+      swiper: swiper,
+    },
+  });
 
 
 
